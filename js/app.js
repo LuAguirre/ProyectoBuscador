fb7e01cbc89e92db95a24e4dc6d3a6de227518fd
Lourdes Aguirre <loumaagui@gmail.com>
2021-06-23 15:51:21 -0600

    Subir el proyecto

---
+++ js/app.js
@@ -0,0 +1,166 @@
+
+const marca = document.getElementById('marca');
+const year = document.getElementById('year');
+const minimo = document.getElementById('minimo');
+const maximo = document.getElementById('maximo');
+const puertas = document.getElementById('puertas');
+const transmision = document.getElementById('transmision');
+const color = document.getElementById('color');
+
+const years = document.createElement('option');
+const max = new Date().getFullYear();
+const min = max - 10;
+
+
+for(let i = max; i >  min; i--) {
+    const option =  document.createElement('option');
+    option.value = i;
+    option.innerText = i;
+    document.getElementById('year').appendChild(option);
+}
+
+const datosBusqueda = {
+    marca : '',
+    year: '',
+    minimo : '',
+    maximo: '',
+    puertas: '',
+    transmision:'',
+    color:''
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    mostrarAutos(autos);
+});
+
+marca.addEventListener('change', e => {
+    datosBusqueda.marca = e.target.value;
+
+
+    filtrarAuto();
+});
+
+year.addEventListener('change', e => {
+    datosBusqueda.year = Number(e.target.value);
+ 
+    filtrarAuto();
+});
+
+minimo.addEventListener('change', e => {
+    datosBusqueda.minimo = Number(e.target.value);
+
+    filtrarAuto();
+});
+
+
+maximo.addEventListener('change', e => {
+    datosBusqueda.maximo = Number(e.target.value);
+
+    filtrarAuto();
+});
+
+
+puertas.addEventListener('change', e => {
+    datosBusqueda.puertas = Number(e.target.value);
+  
+    filtrarAuto();
+});
+
+transmision.addEventListener('change', e => {
+    datosBusqueda.transmision = e.target.value
+    filtrarAuto();
+});
+
+color.addEventListener('change', e => {
+    datosBusqueda.color = e.target.value
+    filtrarAuto();
+});
+
+function limpiarHTML() {
+    const contenedor = document.getElementById('resultado');
+
+
+    while(contenedor.firstChild) {
+        contenedor.removeChild(contenedor.firstChild);
+    }
+}
+
+function mostrarAutos(autos){
+    limpiarHTML();
+
+
+    const contenedor = document.getElementById('resultado');
+
+    autos.forEach(auto => {
+        const autoHTML = document.createElement('p');
+        autoHTML.innerHTML = `
+            <p>${auto.marca} ${auto.modelo} - ${auto.year} - ${auto.puertas} Puertas - Transmisión: ${auto.transmision} - Precio: ${auto.precio} - Color: ${auto.color}</p>
+        `;
+        contenedor.appendChild(autoHTML);
+    })
+}
+function noResultado() {
+    limpiarHTML();
+
+    const noResultado = document.createElement('div');
+    noResultado.classList.add('alerta', 'error');
+    noResultado.appendChild(document.createTextNode('No hay Resultados'));
+    document.getElementById('resultado').appendChild(noResultado);
+}
+
+function filtrarAuto() {
+   const resultado = autos.filter(filtrarMarca).filter(filtrarYear).filter(filtrarMinimo).filter(filtrarMaximo).filter(filtrarPuertas).filter(filtrarTransmision).filter(filtrarColor);
+
+   if(resultado.length){
+        mostrarAutos(resultado);
+   } else {
+       noResultado();
+   }
+}
+
+
+function filtrarMarca(auto) {
+    if(datosBusqueda.marca){
+        return auto.marca === datosBusqueda.marca;
+    } 
+    return auto;
+}
+function filtrarYear(auto) {
+    if(datosBusqueda.year){
+        return auto.year === datosBusqueda.year;
+    }
+    return auto;
+}
+
+function filtrarMinimo(auto) {
+    if(datosBusqueda.minimo){
+        return auto.precio >= datosBusqueda.minimo;
+    }
+    return auto;
+}
+function filtrarMaximo(auto) {
+    if(datosBusqueda.maximo){
+        return auto.precio <= datosBusqueda.maximo;
+    }
+    return auto;
+}
+function filtrarPuertas(auto) {
+    if(datosBusqueda.puertas){
+        return auto.puertas === datosBusqueda.puertas;
+    }
+    return auto;
+}
+
+function filtrarTransmision(auto) {
+    if(datosBusqueda.transmision){
+        return auto.transmision === datosBusqueda.transmision;
+    } 
+    return auto;
+}
+
+function filtrarColor(auto){
+    if(datosBusqueda.color){
+        return auto.color === datosBusqueda.color;
+    } 
+    return  auto;
+}
